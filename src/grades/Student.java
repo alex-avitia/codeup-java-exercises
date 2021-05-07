d4879e05c50b1e07936e99fb771cd8593466019d
Alex Avitia <alex.avitia77@gmail.com>
2021-05-07 14:56:26 -0500

    Finished: Student collections ex

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,27 +4,50 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    private ArrayList<Integer> grades;
+    private ArrayList<Integer> grades = new ArrayList<>();
 
-    public Student(String name, int grade) {
+
+    public Student(String name, int[] grade) {
         this.name = name;
-        this.grades = new ArrayList<>();
+        for (int i = 0; i < grade.length; i++) {
+            addGrade(grade[i]);
+        }
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public void addGrade(int grade) {
         grades.add(grade);
     }
 
+    public double getGradeAverage() {
+        double total = 0;
+
+        for (int i = 0; i < grades.size(); i++ ) {
+            total += grades.get(i);
+        }
+        return total / grades.size();
+    }
+
     // Main
     public static void main(String[] args) {
-        Student kyle = new Student("Kyle", 84);
-        Student michelle = new Student("Michelle", 90);
-        Student ed = new Student("Eduardo", 100);
+
+        Student kyle = new Student("Kyle", new int[]{84, 90, 86});
+        Student michelle = new Student("Michelle", new int[] {90, 90, 100});
+        Student ed = new Student("Eduardo", new int[] {100, 50, 70});
+
+        System.out.print(kyle.getName() + " -- grade = " + kyle.grades);
+        System.out.println(" Average -- " + kyle.getGradeAverage());
+        System.out.print(michelle.getName() + " -- grade = " + michelle.grades);
+        System.out.println(" Average -- " + michelle.getGradeAverage());
+        System.out.print(ed.getName() + " -- grade = " + ed.grades);
+        System.out.println(" Average -- " + ed.getGradeAverage());
 
 
+
+
+//        Test your Student class by adding a main method and creating Student objects. Verify that you can add grades to each object, and that your getGradeAverage method correctly returns the average of the student's grades.
     }
 }
